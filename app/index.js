a41b4bac24614eed578e27c1e6670157a4051827
Ian Carrico <git@iancarrico.com>
2014-11-26 16:55:33 -0600

    Update the syntax, try out 0.18

--- app/index.js
+++ app/index.js
@@ -11,14 +11,19 @@ var shared = require('../shared.js');
 
 var PooleGenerator = yeoman.generators.Base.extend({
   initializing: function () {
+    if (!this.options['skip-welcome-message']) {
+
+      this.log(shared.welcome());
+      this.log(
+        chalk.green(
+          'This generator will create the scaffolding for a full Jekyll site, complete with smarter Gulp settings, and deployment methods. ' + '\n'
+        )
+      );
+    }
+
     this.pkg = require('../package.json');
 
     this.on('end', function () {
-      //////////////////////////////
-      // Move Yo Storage
-      //////////////////////////////
-      fs.renameSync('../.yo-rc.json', '.yo-rc.json');
-
       //////////////////////////////
       // If the --git flag is passed, initialize git and add for initial commit
       //////////////////////////////
@@ -29,60 +34,11 @@ var PooleGenerator = yeoman.generators.Base.extend({
     });
   },
 
-  // Installation tasks to happen before end is called.
-  install: function () {
+  // Prompting tasks.
+  prompting: function() {
     var cb = this.async();
 
-    //////////////////////////////
-    // Install dependencies unless --skip-install is passed
-    //////////////////////////////
-    if (!this.options['skip-install']) {
-
-      // Leavings these as variables in case we decide to use them again.
-      var bower = false;
-      var npm = true;
-
-      if (bower || npm) {
-        this.installDependencies({
-          bower: bower,
-          npm: npm
-        });
-      }
-
-      // Install our bundler components into the .vendor/bundle path, to ensure
-      // no issues if users have differing environments.
-      this.spawnCommand('bundle', ['install', '--path=.vendor/bundle'])
-        .on('close', cb);
-    }
-    else {
-      cb();
-    }
-  }
-});
-
-
-PooleGenerator.prototype.welcome = function() {
-  var cb = this.async();
-  if (!this.options['skip-welcome-message']) {
-
-    this.log(shared.welcome());
-    this.log(
-      chalk.green(
-        'This generator will create the scaffolding for a full Jekyll site, complete with smarter Gulp settings, and deployment methods. ' + '\n'
-      )
-    );
-  }
-
-  cb();
-};
-
-PooleGenerator.prototype.askfor = function() {
-  var cb = this.async();
-
-  // Have Yeoman greet the user.
-  //this.log(shared.welcome());
-
-  var prompts = [
+    var prompts = [
     {
       type: 'string',
       name: 'projectName',
@@ -116,72 +72,110 @@ PooleGenerator.prototype.askfor = function() {
       name: 'projectRepo',
       message: 'What\'s your projects\'s repository?'
     }
-  ];
+    ];
 
-  this.prompt(prompts, function (props) {
-    this.projectName = props.projectName;
-    this.projectSlug = _s.underscored(props.projectName);
-    this.projectUrl = props.projectUrl;
-    this.description = props.description;
-    this.projectRepo = props.projectRepo;
+    this.prompt(prompts, function (props) {
+      this.projectName = props.projectName;
+      this.projectSlug = _s.underscored(props.projectName);
+      this.projectUrl = props.projectUrl;
+      this.description = props.description;
+      this.projectRepo = props.projectRepo;
+
+      // Create our project directory.
+      this.mkdir(this.projectSlug);
+      // We are going to put everything into this sub-root.
+      this.destinationRoot(this.projectSlug);
+
+      this.config.set('projectName', this.projectName);
+      this.config.set('projectSlug', this.projectSlug);
+      this.config.set('projectUrl', this.projectUrl);
+      this.config.set('description', this.description);
+      this.config.set('projectRepo', this.projectRepo);
+
+      // Format date for posts
+      this.currentDate = shared.today();
+      this.currentTime = shared.now();
+
+      cb();
+    }.bind(this));
+  },
+
+  // Configuration files.
+  configuring: function() {
+    // Base settings files.
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('jshintrc', '.jshintrc');
+    this.copy('gitignore', '.gitignore');
+    this.copy('bowerrc', '.bowerrc');
+  },
+
+  // Default task level.
+  app: function() {
+
+    // Run with the Corona Sass Generator. We are putting this in the
+    // intializing because we want to override a few things.
+    this.composeWith('corona:app', {
+      options: {
+        cssDir: 'css',
+        sassDir: '_sass',
+        fontsDir: 'fonts',
+        'skip-install': true,
+        'skip-welcome-message': true
+      }
+    });
+
+    // Other files
+    this.copy('gulpfile.js', 'gulpfile.js');
+    this.copy('index.html', 'index.html');
+    this.copy('about.md', 'about.md');
+    this.copy('feed.xml', 'feed.xml');
+    this.copy('_config.dev.yml', '_config.dev.yml');
+    this.copy('404.md', '404.md');
 
 
-    this.config.set('projectName', this.projectName);
-    this.config.set('projectSlug', this.projectSlug);
-    this.config.set('projectUrl', this.projectUrl);
-    this.config.set('description', this.description);
-    this.config.set('projectRepo', this.projectRepo);
+    var keep = ['_images', 'fonts', 'js'];
+    for (var i in keep) {
+      this.copy('gitkeep', '' + keep[i] + '/.gitkeep');
+    }
 
-    // Format date for posts
-    this.currentDate = shared.today();
-    this.currentTime = shared.now();
-
-    cb();
-  }.bind(this));
-};
-
-PooleGenerator.prototype.app = function() {
-  // Create our project directory.
-  this.mkdir(this.projectSlug);
-
-  // Set our destination to be the new directory.
-  this.destinationRoot(this.projectSlug);
-
-  // Base settings files.
-  this.copy('editorconfig', '.editorconfig');
-  this.copy('jshintrc', '.jshintrc');
-  this.copy('gitignore', '.gitignore');
-  this.copy('bowerrc', '.bowerrc');
-
-  // Other files
-  this.copy('gulpfile.js', 'gulpfile.js');
-  this.copy('index.html', 'index.html');
-  this.copy('about.md', 'about.md');
-  this.copy('feed.xml', 'feed.xml');
-  this.copy('_config.dev.yml', '_config.dev.yml');
-  this.copy('404.md', '404.md');
-  this.copy('Gemfile', 'Gemfile');
-  this.copy('config.rb', 'config.rb');
+    // Folders that will be directly copied over.
+    this.directory('_includes', '_includes');
+    this.directory('_layouts', '_layouts');
+    this.directory('_data', '_data');
+    this.directory('_plugins', '_plugins');
 
 
-  var keep = ['_images', 'fonts', 'js'];
-  for (var i in keep) {
-    this.copy('gitkeep', '' + keep[i] + '/.gitkeep');
+    // Files that need to be templated.
+    this.template('_package.json', 'package.json');
+    this.template('_config.yml', '_config.yml');
+    this.template('_posts/firstpost.md', '_posts/' + this.currentDate + '-welcome-to-poole.md');
+  },
+
+  // Conflicts task, where we ensure we have The Right Stuff™.
+  write: function() {
+
+    // We want to use our Gemfile, because it has more in it then just the Sass
+    // and Compass files.
+    this.copy('Gemfile', 'Gemfile');
+  },
+
+  // Installation tasks to happen before end is called.
+  install: function () {
+    //////////////////////////////
+    // Install dependencies unless --skip-install is passed
+    //////////////////////////////
+    if (!this.options['skip-install']) {
+
+      this.installDependencies({
+        npm: true
+      });
+
+      // Install our bundler components into the .vendor/bundle path, to ensure
+      // no issues if users have differing environments.
+      this.spawnCommand('bundle', ['install', '--path=.vendor/bundle'])
+        .on('close', cb);
+    }
   }
-
-  // Folders that will be directly copied over.
-  this.directory('_includes', '_includes');
-  this.directory('_layouts', '_layouts');
-  this.directory('_sass', '_sass');
-  this.directory('_data', '_data');
-  this.directory('_plugins', '_plugins');
-
-
-  // Files that need to be templated.
-  this.template('_package.json', 'package.json');
-  //this.template('_bower.json', 'bower.json');
-  this.template('_config.yml', '_config.yml');
-  this.template('_posts/firstpost.md', '_posts/' + this.currentDate + '-welcome-to-poole.md');
-};
+});
 
 module.exports = PooleGenerator;
